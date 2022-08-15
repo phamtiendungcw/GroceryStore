830104272768925ea17774c2c260f29598af4f33
Phạm Tiến Dũng <56197293+phamtiendungcwit@users.noreply.github.com>
2022-08-15 10:23:39 +0700

    Product API controller Post Put Get Delete

---
+++ Grocery.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Grocery.Services.ProductAPI.Models;
+using Grocery.Services.ProductAPI.Models.DTO;
+using Grocery.Services.ProductAPI.Reponsitory;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Grocery.Services.ProductAPI.Controllers
+{
+    [Route("api/products")]
+    public class ProductAPIController : ControllerBase
+    {
+        protected ResponseDTO _response;
+        private IProductRepository _productRepository;
+
+        public ProductAPIController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+            this._response = new ResponseDTO();
+        }
+
+        [HttpGet]
+        public async Task<object> Get()
+        {
+            try
+            {
+                IEnumerable<ProductDTO> productDtos = await _productRepository.GetProducts();
+                _response.Result = productDtos;
+            }
+            catch (Exception e)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { e.ToString() };
+            }
+
+            return _response;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<object> Get(int id)
+        {
+            try
+            {
+                ProductDTO productDto = await _productRepository.GetProductById(id);
+                _response.Result = productDto;
+            }
+            catch (Exception e)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { e.ToString() };
+            }
+
+            return _response;
+        }
+
+        [HttpPost]
+        public async Task<object> Post([FromBody] ProductDTO productDto)
+        {
+            try
+            {
+                ProductDTO model = await _productRepository.CreateUpdateProduct(productDto);
+                _response.Result = model;
+            }
+            catch (Exception e)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { e.ToString() };
+            }
+
+            return _response;
+        }
+
+        [HttpPut]
+        public async Task<object> Put([FromBody] ProductDTO productDto)
+        {
+            try
+            {
+                ProductDTO model = await _productRepository.CreateUpdateProduct(productDto);
+                _response.Result = model;
+            }
+            catch (Exception e)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { e.ToString() };
+            }
+
+            return _response;
+        }
+
+        [HttpDelete]
+        public async Task<object> Delete(int id)
+        {
+            try
+            {
+                bool isSuccess = await _productRepository.DeleteProduct(id);
+                _response.Result = isSuccess;
+            }
+            catch (Exception e)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { e.ToString() };
+            }
+
+            return _response;
+        }
+    }
+}
